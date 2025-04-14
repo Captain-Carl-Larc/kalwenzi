6445a978056fb0165876bb89bdaebf64dfc006a6
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-04-14 22:33:02 +0300

    populated home page sections

--- carl/src/app/page.js
+++ carl/src/app/page.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 export default function Home() {
   return(
     <div>
-      <h1 className='text-6xl'>
-        Hello world
-      </h1>
-      <Link href='/about'>
-        About
-      </Link>
+      <section className='text-6xl'>
+        hero section
+      </section>
+      <section >
+        About section
+      </section>
     </div>
   )
 }
