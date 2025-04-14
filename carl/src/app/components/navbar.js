3bf62a7ae9f323bf9673f52516c0251c966923d8
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-04-14 15:42:26 +0300

    navbar update

--- carl/src/app/components/navbar.js
+++ carl/src/app/components/navbar.js
@@ -1,23 +1,38 @@
-//'use client'
+'use client'
 import Link from 'next/link'
-//import useState from 'react'
+import { useState } from 'react';
 
 function Navbar() { 
-    const isOpen = false
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const closeIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+    <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+  </svg>
+  
+    const openIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 font-extrabold text-gray-950">
+    <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+  </svg>
+  
+    // setIsMenuOpen(!isMenuOpen)
+    
     return ( 
-        <header className="bg-blue-700 margin-0 padding-0">
-            <div id="logo-container">
-                <p>
+        <header className="bg-blue-700 margin-0 padding-0 flex justify-between h-16 items-center relative md:static">
+            <div id="logo-container" className='pl-4'>
+                <p className='font-bold text-3xl'>
                     Karl
                 </p>
             </div>
-            <div>
-               <h2 >
-                {isOpen?'Open':'closed'}
-               </h2>
+            <div className='absolute bg-amber-300 md:static top-4 right-4 md:hidden font-extrabold'>               
+                {
+                   isMenuOpen?(
+                    closeIcon
+                   ):(
+                    openIcon
+                   )
+                }               
             </div>
-            <nav>
-                <ul className='flex flex-col gap-y-2 sm:flex-row sm:gap-x-4 sm:gap-y-2'>
+            <nav className='md:pr-4'>
+                <ul className='hidden md:flex md:gap-x-4 '>
                     <li>
                         <Link
                             href={`/`}
